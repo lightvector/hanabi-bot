a8e717c2156834259716b2c98b1673ae9a06d18b
Sawyer Tabony <sawyertabony@gmail.com>
2015-09-10 23:14:31 +0000

    interpreter.mli written

--- lib/interpreter.mli
+++ lib/interpreter.mli
@@ -2,27 +2,25 @@ open Core.Std
 open Hanabi_types
 open Game
 
-module Tag = struct
-  type quality =
+module Tag : sig
+  type t =
   (* At the time of a turn, hints or signals will be assigned exactly one of these
      qualities.
      For hints, maybe based on public information, so it cannot be mis-assigned? *)
   | Playable
   | Danger
   | Discardable
-  (* dunno about this one *)
-  | Valuable
-  with sexp
-
-  type t =
-  | Hinted of quality * State.t * Player_id.t * Hint.hint * int
-  (* Examples:
-     Playable - Crossovers and such
-     Dangerous - Say when bombing, assign to everything behind bomb
-     Discardable - Most discardable card in hand,
-     rightmost opponent hints playable of person beyond you when you have no play *)
-  | Signaled of quality * State.t * Player_id.t
-  (* Unsure if State.t, Player_id.t here is necessary *)
-  | Anti_hinted of Hint.hint
+  | Hinted
   with sexp
 end
+
+module Beliefs : sig
+  type t =
+    { player : Player_id.t
+    ; tags   : Tag.t list Card_id.Map.t
+    }
+end
+
+type t = Beliefs.t Player_id.Map.t
+
+val interpret : t -> State.t -> Turn.t -> t
